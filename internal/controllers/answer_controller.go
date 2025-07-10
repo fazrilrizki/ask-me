dd238560861ff0ae440253b75f864a4f92f08216
Fazril <fazrilrizkitantoadji@gmail.com>
2025-07-10 17:25:00 +0700

    fix: add params to url answer question api

--- internal/controllers/answer_controller.go
+++ internal/controllers/answer_controller.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
 	"self-project/ask-me/internal/models"
 	"self-project/ask-me/pkg/database"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 )
@@ -11,11 +12,18 @@ import (
 func StoreAnswer(c *gin.Context) {
 	var answer models.Answer
 
+	id, err := strconv.Atoi(c.Param("question_id"))
+	if err != nil {
+		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"message": "Invalid question_id"})
+		return
+	}
+
 	if err := c.ShouldBindJSON(&answer); err != nil {
 		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"message": err.Error()})
 		return
 	}
 
+	answer.QuestionId = int64(id)
 	database.DB.Create(&answer)
 	c.JSON(http.StatusCreated, gin.H{"answer": answer})
 }

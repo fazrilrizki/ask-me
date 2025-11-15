54783c3c851f5a89378ffaa3c1f4c0b999622fb9
fazril <fazril@dinamika.ac.id>
2025-11-15 22:46:55 +0700

    fix: foreign key question in question vote table

--- internal/models/question.go
+++ internal/models/question.go
@@ -3,7 +3,7 @@ package models
 import "time"
 
 type Question struct {
-	Id        int64     `gorm:"primaryKey" json:"id"`
+	Id        uint      `gorm:"primaryKey" json:"id"`
 	UserName  string    `gorm:"type:varchar(255)" json:"user_name"`
 	Question  string    `gorm:"type:varchar(255)" json:"question"`
 	Answers   []Answer  `gorm:"foreignKey:QuestionId" json:"answers"`
--- internal/models/question_vote.go
+++ internal/models/question_vote.go
@@ -1,10 +1,13 @@
 package models
 
+import "time"
+
 type QuestionVote struct {
-    ID         uint   `gorm:"primaryKey"`
-    VisitorID  string `json:"visitor_id" gorm:"size:255;uniqueIndex:idx_visitor_question"`
-    QuestionID uint   `json:"question_id" gorm:"uniqueIndex:idx_visitor_question"`
-    VoteType   string `json:"vote_type" gorm:"type:enum('up','down')"`
-	CreatedAt time.Time `gorm:"autoCreateTime" json:"created_at"`
-	UpdatedAt time.Time `gorm:"autoUpdateTime" json:"updated_at"`
+	ID         uint   `gorm:"primaryKey"`
+	VisitorID  string `json:"visitor_id" gorm:"size:255;uniqueIndex:idx_visitor_question"`
+	QuestionID uint   `json:"question_id" gorm:"uniqueIndex:idx_visitor_question"`
+	Question   Question
+	VoteType   string    `json:"vote_type" gorm:"type:enum('up','down')"`
+	CreatedAt  time.Time `gorm:"autoCreateTime" json:"created_at"`
+	UpdatedAt  time.Time `gorm:"autoUpdateTime" json:"updated_at"`
 }
